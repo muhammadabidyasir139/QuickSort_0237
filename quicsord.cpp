82d0dbefb557c5df15a2fb88ad379deacf8b584d
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2024-03-28 23:12:26 +0700

    membuat flow input

--- quicsord.cpp
+++ quicsord.cpp
@@ -7,6 +7,17 @@ int cmp_count = 0;
 int mov_cpunt = 0;
 int n;
 
+void input() {
+    while (true) {
+        cout << "masukan panjang elemen array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\n maksimum panjanga array adalah 20" << endl;
+    }
+}
 
 int main () {
 
